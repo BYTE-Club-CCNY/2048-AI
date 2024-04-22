6eee76374a30337d53b6ac516d46b1e760f05826
Abrar Habib <abrarhabib285@gmail.com>
2024-04-22 17:46:55 -0400

    added model.py to model branch

---
+++ game/model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+class Linear_QNet(nn.Module):
+# nn module
+
+class QTrainer:
+# train_step function, init function
+    
